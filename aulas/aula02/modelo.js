1b7bc920634af6eac864df097ad3f6cd300cf8c9
Joao Pedro Trindade <joao.trindade@iesb.edu.br>
2024-02-27 21:57:30 -0300

    novas mudanças

---
+++ aulas/aula02/modelo.js
@@ -0,0 +1,10 @@
+class Produto{
+    constructor(nome,preco){
+        this.nome=nome;
+        this.preco=preco;
+    }
+    toString(){
+        return `{${this.nome} - ${this.preco}}`
+    }
+}
+module.exports = Produto
